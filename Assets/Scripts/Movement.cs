12af1cdc8890a73daed3ac0c358e8b146012fa5d
Dishant Mohapatra <dishantmohapatrak20@gmail.com>
2023-01-30 19:37:15 +0530

    clean up assets by creating individual folders

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if(Input.GetKey(KeyCode.Space))
+            rb.AddRelativeForce(Vector3.up);
+        
+    }
+
+    void ProcessRotation()
+    {
+        if(Input.GetKey(KeyCode.A))
+            Debug.Log("Rotate left");
+        else if(Input.GetKey(KeyCode.D))
+            Debug.Log("Rotate Right");
+    }
+}
