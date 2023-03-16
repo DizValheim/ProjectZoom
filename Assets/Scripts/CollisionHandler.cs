c266f7a5283c6d06ba6f8efeccc3c87416311fdb
Dishant Mohapatra <dishantmohapatrak20@gmail.com>
2023-03-16 10:18:02 +0530

    Add delays to Crash and Success Sequences

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
+
+    [SerializeField] float levelLoadDelay = 1f;
    
     void OnCollisionEnter(Collision other) 
     {
@@ -16,19 +18,33 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("Fuel++");
                 break;
             case "Finish":
-                LoadNextLevel();
+                StartSuccessSequence();
                 Debug.Log("Level Complete!");
                 break;
             default:
-                ReloadLevel();
-                Debug.Log("Whatever!");
+                StartCrashSequence();
                 break;
         }
 
     }
 
+    void StartCrashSequence()
+    {
+        //todo add crash SFX
+        //todo add particle effects upon crash
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+    void StartSuccessSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
     void LoadNextLevel()
     {
+
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         int nextSceneIndex = currentSceneIndex + 1;
         if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
